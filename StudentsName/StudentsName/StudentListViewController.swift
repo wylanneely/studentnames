f49ab764c4b8621f55c24e4525fbcbae642b2aec
wylanneely@gmail.com <aliamneely@ALIAs-MacBook-Pro.local>
2017-04-19 10:46:37 -0600

    Set up the view

---
+++ StudentsName/StudentsName/StudentListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StudentListViewController.swift
+//  StudentsName
+//
+//  Created by ALIA M NEELY on 4/19/17.
+//  Copyright © 2017 Wylan. All rights reserved.
+//
+
+import UIKit
+
+class StudentListViewController: UIViewController {
+
+
+    
+    
+
+}
