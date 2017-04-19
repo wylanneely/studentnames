d03d77068f7e58f35f7468553b2b497677ffbed5
wylanneely@gmail.com <aliamneely@ALIAs-MacBook-Pro.local>
2017-04-19 10:39:57 -0600

    YO

---
+++ StudentsName/StudentsName/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  StudentsName
+//
+//  Created by ALIA M NEELY on 4/19/17.
+//  Copyright © 2017 Wylan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+   
+}
+
